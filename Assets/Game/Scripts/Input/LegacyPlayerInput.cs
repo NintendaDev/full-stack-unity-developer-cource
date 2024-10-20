d2b64a5c713207470fddd09c950011f10a1b2cf3
Dmitrii Vasilev <d.vasilev.gamedev@gmail.com>
2024-10-20 10:15:51 +0700

    Code formatting fix

--- Assets/Game/Scripts/Input/LegacyPlayerInput.cs
+++ Assets/Game/Scripts/Input/LegacyPlayerInput.cs
@@ -3,23 +3,22 @@
 namespace SpaceInvaders.PlayerInput
 {
     public sealed class LegacyPlayerInput : IPlayerInput
-
     {
-    private readonly Vector2 _leftDirection = new(-1, 0);
-    private readonly Vector2 _rightDirection = new(1, 0);
-    private readonly Vector2 _defaultDirection = Vector2.zero;
+        private readonly Vector2 _leftDirection = new(-1, 0);
+        private readonly Vector2 _rightDirection = new(1, 0);
+        private readonly Vector2 _defaultDirection = Vector2.zero;
 
-    public bool IsPressedFire() => Input.GetKeyDown(KeyCode.Space);
+        public bool IsPressedFire() => Input.GetKeyDown(KeyCode.Space);
 
-    public Vector2 ReadMoveDirection()
-    {
-        if (Input.GetKey(KeyCode.LeftArrow))
-            return _leftDirection;
+        public Vector2 ReadMoveDirection()
+        {
+            if (Input.GetKey(KeyCode.LeftArrow))
+                return _leftDirection;
 
-        if (Input.GetKey(KeyCode.RightArrow))
-            return _rightDirection;
+            if (Input.GetKey(KeyCode.RightArrow))
+                return _rightDirection;
 
-        return _defaultDirection;
-    }
+            return _defaultDirection;
+        }
     }
 }
